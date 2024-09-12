88d1974e4c1ebb54956ec287805725264c8daaf8
kalistos1 <ucktech1@gmail.com>
2024-09-12 18:32:48 +0100

    norminalized inventory db and create model forms

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import (
      Equipment, ConsumableItem, EquipmentUsageLog, 
-    InsurancePolicy, EquipmentAuditLog, InspectionChecklist
+    InsurancePolicy, EquipmentAuditLog, InspectionChecklist, Alert
 )
 
 
@@ -11,3 +11,4 @@ admin.site.register(EquipmentUsageLog)
 admin.site.register(InsurancePolicy)
 admin.site.register(EquipmentAuditLog)
 admin.site.register(InspectionChecklist)
+admin.site.register(Alert)
