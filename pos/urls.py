fa5b71cf54f4db377ed3cc95b4d260230f339b7f
kalistos1 <ucktech1@gmail.com>
2024-09-17 12:51:18 +0100

    setup pos with htmx

--- pos/urls.py
+++ pos/urls.py
@@ -10,6 +10,14 @@ urlpatterns = [
 htmx_views =[
     path('ordering/',  views.pos_index, name='products'),
     path('products/<slug:slug>/', views.category_products_view, name='category-products'),
+    path('cart/', views.cart_view, name='cart_view'),
+    path('cart/add/<int:pk>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/increase/<int:item_id>/', views.increase_item_quantity, name='increase_item_quantity'),
+    path('cart/decrease/<int:item_id>/', views.decrease_item_quantity, name='decrease_item_quantity'),
+    path('cart/update-total/', views.update_cart_total, name='update_cart_total'),
+    path('cart/checkout/', views.checkout_view, name='checkout_view'),
+    path('cart/process/', views.process_checkout, name='process_checkout'),
+
 ]
 
 urlpatterns += htmx_views
